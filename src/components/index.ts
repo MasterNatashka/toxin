0628f3f54c1c4db91fa1a74005843cf306f1cb14
Natalia Volos <masternatashka@gmail.com>
2023-01-05 23:58:42 +0300

    Update webpack config; add layouts

--- src/components/index.ts
+++
@@ -1,2 +0,0 @@
-import './BulletList';
-import './ColorBox';
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,44 +1,74 @@
-import * as path from 'path';
-import * as webpack from 'webpack';
-import HtmlPlugin from 'html-webpack-plugin';
-import MiniCSSExtractPlugin from 'mini-css-extract-plugin';
+import * as fs from "fs";
+import * as path from "path";
+import * as webpack from "webpack";
+import HtmlPlugin from "html-webpack-plugin";
+import MiniCSSExtractPlugin from "mini-css-extract-plugin";
 // @ts-ignore
-import PugPlugin from 'pug-plugin';
+import PugPlugin from "pug-plugin";
+
+const PAGES_DIR = path.resolve(__dirname, "src/pages");
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .map((page) => page.replace(/\.[^/.]+$/, ""));
+const PATHS = {
+  src: path.resolve(__dirname, "src"),
+  dist: path.resolve(__dirname, "dist"),
+};
+const isDevMode = process.env.NODE_ENV === "development";
+
+const entryPages = PAGES.reduce<Record<string, string>>((pages, page) => {
+  pages[page] = `${PAGES_DIR}/${page}/${page}.pug`;
+  return pages;
+}, {});
+
+const filename = (ext: string) =>
+  isDevMode ? `${ext}/[name].${ext}` : `${ext}/[name].[contenthash].${ext}`;
 
 const config: webpack.Configuration = {
-  mode: 'production',
-  entry: './src/index.ts',
+  mode: isDevMode ? "development" : "production",
+  entry: entryPages,
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.[hash].js',
+    path: PATHS.dist,
+    filename: filename("js"),
   },
   module: {
     rules: [
       {
         test: /\.pug$/,
-        include: path.resolve(__dirname, 'src'),
+        include: PATHS.src,
         use: PugPlugin.loader,
       },
       {
         test: /\.scss$/,
-        include: path.resolve(__dirname, 'src'),
-        use: [MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        include: PATHS.src,
+        use: ["css-loader", "postcss-loader", "sass-loader"],
       },
     ],
   },
   plugins: [
-    new HtmlPlugin({ template: 'src/index.pug' }),
-    new HtmlPlugin({ filename: 'colors.html', template: 'src/pages/colors/colors.pug' }),
-    new HtmlPlugin({ filename: 'landing.html', template: 'src/pages/landing/landing.pug' }),
-    new MiniCSSExtractPlugin({
-      filename: '[name].[hash].css',
+    new PugPlugin({
+      clean: isDevMode,
+      js: {
+        filename: filename("js"),
+      },
+      css: {
+        filename: filename("css"),
+      },
     }),
+    // new HtmlPlugin({ template: 'src/index.pug' }),
+    // new HtmlPlugin({ filename: 'colors.html', template: 'src/pages/colors/colors.pug' }),
+    // new HtmlPlugin({ filename: 'landing.html', template: 'src/pages/landing/landing.pug' }),
+    // new MiniCSSExtractPlugin({
+    //   filename: "[name].[contenthash].css",
+    // }),
   ],
   resolve: {
     alias: {
-      pages: path.resolve(__dirname, 'src/pages/'),
-      styles: path.resolve(__dirname, 'src/styles/'),
-      components: path.resolve(__dirname, 'src/components/'),
+      // pages: `path.resolve(__dirname, "src/pages/")`,
+      layouts: path.resolve(__dirname, "src/layouts/"),
+      pages: path.resolve(__dirname, "src/pages/"),
+      styles: path.resolve(__dirname, "src/styles/"),
+      components: path.resolve(__dirname, "src/components/"),
     },
   },
 };
